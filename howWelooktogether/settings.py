3dfd178b4cb7f213767933c20430de52944920cc
rishu <divyanshkumarworks@gmail.com>
2023-07-08 20:47:17 +0530

    changed secret key location

--- howWelooktogether/settings.py
+++ howWelooktogether/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import DJANGO_SECRET_KEY
 
 import os
 
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1l6gj06p#d+!jrr+g&eb^(-)e_o6$xc#issywbs@u!dk9+!!j2'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
